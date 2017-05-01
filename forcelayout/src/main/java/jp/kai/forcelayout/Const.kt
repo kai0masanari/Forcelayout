f0f2d46cae7386cab8a6081dc73a7c3a1dd4ca40
kai0masanari <k-toyama@sumilab.org>
2017-05-01 19:44:56 +0900

    add TODO

---
+++ forcelayout/src/main/java/jp/kai/forcelayout/Const.kt
@@ -0,0 +1,5 @@
+package jp.kai.forcelayout
+
+/**
+ * Created by kai on 2017/05/01.
+ */
--- forcelayout/src/main/java/jp/kai/forcelayout/Forcelayout.java
+++ forcelayout/src/main/java/jp/kai/forcelayout/Forcelayout.java
@@ -386,6 +386,8 @@ public class Forcelayout extends View{
         //spring like attractive forces
         private static int distance = 300; //distance between nodes
         private static double gravity = 0.04;
+
+        //TODO Constに移す
         private static double bounce = 0.08; //
         private static double attenuation = 0.7;//0.9; //
         private static double coulomb = 680; //

e9e819276e1d7a051414dadd78607a02a7059e42
kai0masanari <k-toyama@sumilab.org>
2017-05-14 13:34:02 +0900

    implement Link and Node Properties class

--- forcelayout/src/main/kotlin/jp/kai/forcelayout/properties/LinkProperty.kt
+++ forcelayout/src/main/kotlin/jp/kai/forcelayout/properties/LinkProperty.kt
@@ -1,9 +1,27 @@
 package jp.kai.forcelayout.properties
 
+import java.lang.Integer.parseInt
+
 /**
  * Created by kai on 2017/05/14.
  */
 
 class LinkProperty {
+    fun prepare(): LinkProperty {
+        return this
+    }
 
+    fun syle(color:String ,width: Float): LinkProperty {
+        GraphStyle.linkColor = parseInt(color, 16)
+        GraphStyle.linkWidth = width
+
+        return this
+    }
+
+    fun syle(color:Int ,width: Float): LinkProperty {
+        GraphStyle.linkColor = color
+        GraphStyle.linkWidth = width
+
+        return this
+    }
 }
--- forcelayout/src/main/kotlin/jp/kai/forcelayout/properties/NodeProperty.kt
+++ forcelayout/src/main/kotlin/jp/kai/forcelayout/properties/NodeProperty.kt
@@ -1,9 +1,31 @@
 package jp.kai.forcelayout.properties
 
+import java.lang.Integer.parseInt
+
 /**
  * Created by kai on 2017/05/14.
  */
 
 class NodeProperty {
+    fun prepare(): NodeProperty {
+        return this
+    }
 
+    fun size(width: Int): NodeProperty{
+        GraphStyle.nodesWidth = width
+
+        return this
+    }
+
+    fun syle(color: String): NodeProperty {
+        GraphStyle.nodeColor = parseInt(color, 16)
+
+        return this
+    }
+
+    fun syle(color: Int): NodeProperty {
+        GraphStyle.nodeColor = color
+
+        return this
+    }
 }
